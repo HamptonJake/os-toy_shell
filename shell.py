b75c1a6860e9d6f65e294ee9e95031714aeccd75
HamptonJake <49708455+HamptonJake@users.noreply.github.com>
2023-03-08 02:24:29 -0700

    Add files via upload

--- shell.py
+++ shell.py
@@ -10,10 +10,11 @@ pid = os.getpid()
 
 
 
+
 #Print the prompt to the user for every entry until exit
 while True:
 	if 'PS1' not in os.environ:
-		print('PS1 not set.\nDefault prompt will be: $.')
+		#print('PS1 not set.\nDefault prompt will be: $.')
 		path = os.getcwd() + '$'
 		#Print to standard out
 		os.write(1, path.encode())
@@ -29,6 +30,21 @@ while True:
 	if 'exit' in input_stream:
 		print('Closing Shell')
 		sys.exit(0)
+	elif input_stream[0] == 'cd':
+		print('Attempt to change directory\n')
+		try:
+			os.chdir(input_stream[1])
+			print('Path updated successfully')
+		except:
+			print('Path does not exist or arguments missing')
+			#os.write(2, f'Path does not exist or arguments missing')
+		
+	elif '|' in input_stream:
+		print('Attempt to pipe\n')
+		#Used to move data between commands
+		#Command1 -> Command2
+		#Separate both sides of the pipe
+		
 		
 		
 		
