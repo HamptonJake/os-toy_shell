0a995e09b19fad484f7c51a74ab95fcdf3ab134b
HamptonJake <49708455+HamptonJake@users.noreply.github.com>
2023-03-29 14:29:35 -0600

    Update shell.py

--- shell.py
+++ shell.py
@@ -56,21 +56,80 @@ while True:
 		
 		
 		if the_fork = 0:
+			dup_fd_write = pip_w
+			#Close previous write
+			os.close(1)
+			os.dup(dup_fd_write)
+			os.set_inheritable(1, True)
 			
-			
-			#Does not work
-			for fd in (pip_r, pip_w):
-				os.execve
-			sys.exit(1)
-			
+			#exec
+			for f_descript in (pip_w, pip_r)
+				#executing
+				for dir in re.split(':', os.environ['PATH']):
+					executable = "%s/%s" % (dir, path[0])
+					try:
+						os.execve(executable, pip_write_path, os.environ)
+					except:
+						sys.exit(1)
 			
 		elif the_fork > 0:
-			#Not child
+			pipe_helper(pipe_read, pipe_write, r, 0)
+			dup_fd_read = pip_r
+			#Close previous read
+			os.close(0)
+			os.dup(dup_fd_read)
+			os.set_inheritable(0, True)
 			
-			
+
+			#exec
+			for f_descript in (pip_w, pip_r)
+				#executing
+				for dir in re.split(':', os.environ['PATH']):
+					executable = "%s/%s" % (dir, path[0])
+					try:
+						os.execve(executable, pip_read_path, os.environ)
+					except:
+						sys.exit(1)
+
 		else:
 			#Fork error
 			sys.exit(1)
+			
+	else:
+		#Redirect Section
+		print('Redirection begins')
+
+		redir_fork = os.fork()
+
+		if redir_fork < 0:
+			#Error
+			sys.exit(1)
+
+		elif redir_fork == 0:
+			#Check directions of redirection
+			if '>' in input_stream:
+				# Redirect to fd1 output
+				print('Redirect: >')
+				os.close(1)
+				#If file doesn't exist, create it
+				os.open(input_stream[2], os.O_CREAT | os.O_WRONLY)
+				# Inherit with flag code #1
+				os.set_inheritable(1, True)
+				input_stream = input_stream[:1]
+			elif '<' in input_stream:
+				# Redirect to fd1 output
+				print('Redirect: <')
+				os.close(0)
+				os.open(input_stream[2], os.O_CREAT | os.O_WRONLY)
+				# Inherit with flag code #0
+				os.set_inheritable(0, True)
+				input_stream = input_stream[:1]
+			    # Search each directory in PATH
+			
+			for dir in re.split(":", os.environ['PATH']):
+				redir_exectuable = "%s/%s" % (dir, path[0])
+				try:
+					os.execve()
 			
 		
 		
